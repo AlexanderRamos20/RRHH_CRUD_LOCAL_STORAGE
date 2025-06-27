8495fac8afc0f7f7cd6e718dfa2171b1a8e8b738
Alexander Ramos <ramos.olivera.alexander@gmail.com>
2025-06-27 15:36:49 -0400

    implementa navbar funcional

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/navbar'
+import Footer from './components/footer'
+import Crear from './pages/paginaCrear'
 
 
 function App() {
-  
+    return(
+        <Router>
+            <Navbar/>
+            <Routes>
+                <Route path="/" element={<listar/>}/>
+                <Route path="/crear" element={<Crear />}/>
+                <Route path="/editar" element={<editar />}/>
+                <Route path="/eliminar" element={<eliminar />}/> 
+            </Routes>
+            <Footer/>    
+        </Router>
+    );
 }
 
 export default App
---
+++ src/pages/paginaCrear.jsx
@@ -0,0 +1,6 @@
+
+function crear (){
+
+}
+
+export default crear
